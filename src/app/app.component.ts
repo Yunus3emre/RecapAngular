dd05bddccdddd1408a9311558c2328be3d8141f1
Yunus Emre Ayan <67464029+Yunus3emre@users.noreply.github.com>
2021-03-25 11:22:16 +0300

    commit1

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Recap';
+  user:string ="Yunus Emre"
+  car:any={carId:1,modelYear:2021,dailyPrice:150,description:"Dizel"}
+}
